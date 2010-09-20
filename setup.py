2e1d4a1b64a7465a995dfc54bf6e9c95b355815b
Reinout van Rees <reinout@vanrees.org>
2010-09-20 10:42:17 +0200

    Skeleton generated with nensskel

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+version = '0.1dev'
+
+long_description = '\n\n'.join([
+    open('README.txt').read(),
+    open('TODO.txt').read(),
+    open('CREDITS.txt').read(),
+    open('CHANGES.txt').read(),
+    ])
+
+install_requires = [
+    'setuptools',
+    ],
+
+tests_require = [
+    ]
+
+setup(name='createcoverage',
+      version=version,
+      description="TODO",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords=[],
+      author='TODO',
+      author_email='TODO@nelen-schuurmans.nl',
+      url='',
+      license='GPL',
+      packages=['createcoverage'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require = {'test': tests_require},
+      entry_points={
+          'console_scripts': [
+          ]},
+      )
